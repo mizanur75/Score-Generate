735e6813960b2ab65570d57c25c3134c664a3def
Mizanur Rahman <mizanurrahman615@gmail.com>
2024-07-08 16:46:41 +0600

    score page show done

--- index.php
+++ index.php
@@ -69,7 +69,7 @@
 
       <!-- Form 3 -->
       <form id="form3" action="cust_details.php" method="POST" enctype="multipart/form-data" style="display: none;">
-      <div class="row">
+        <div class="row">
           <div class="input-group mb-3">
             <input type="file" name="excel_file" class="form-control" id="inputGroupFile02">
             <label class="input-group-text" for="inputGroupFile02">Upload</label>
@@ -79,6 +79,8 @@
       </form>
 
     </div>
+
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
     <script>
--- score_generate.php
+++ score_generate.php
@@ -55,8 +55,58 @@ if ($variable_coefficient_result && $scaled_inputs_result) {
         $result += number_format((float)$variable_coefficients[$i], 2) * number_format((float)$scaled_inputs[0][$i], 2);
     }
 
-    var_dump($result - (-.086)); exit;
+    $score = ($result - (-.086));
+    // var_dump($score); exit;
 
 } else {
    echo "Error fetching data: " . $conn->errorInfo();
 }
+
+  session_start(); 
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Generated Score</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <h1 class="text-center m-5">Generated Score</h1>
+      <?php
+        if(isset($_SESSION['success'])){
+        ?>
+          <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <?php echo $_SESSION['success']; ?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+       
+       <?php 
+       }
+        unset($_SESSION['success']);
+        
+      ?>
+      <?php
+        if(isset($_SESSION['error'])){
+        ?>
+          <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <?php echo $_SESSION['error']; ?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+       
+       <?php 
+       }
+         unset($_SESSION['error']);
+      ?>
+
+      <!-- Buttons to show form -->
+      <div class="row">
+        <h1 class="text-center">Generated Score is: <span class="badge bg-success"><?php echo $result; ?></span> </h1>
+      </div>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
